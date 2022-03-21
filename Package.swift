7c50474f3887f9484b525f68221cbddeb73dd132
Corey Roberts <croberts22@utexas.edu>
2022-03-21 08:55:43 -0500

    Updating packages.

--- Package.swift
+++ Package.swift
@@ -1,24 +1,23 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
-    name: "Makoto",
+    name: "App",
+    products: [
+      .library(name: "DangerDeps", type: .dynamic, targets: ["App"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/danger/swift.git", from: "1.6.4"),
-
+        .package(url: "https://github.com/danger/swift.git", from: "3.12.3"),
         // Danger Plugins
-        .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary.git", from: "0.1.0")
+        .package(url: "https://github.com/f-meloni/danger-swift-xcodesummary.git", from: "1.2.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "DangerDependencies",
-                dependencies: ["Danger", "DangerXCodeSummary"],
-                path: "Sources/DangerDependencies"),
-        .testTarget(name: "dangerTests",
-                    dependencies: ["Danger", "DangerXCodeSummary"])
+        .target(
+            name: "App",
+            dependencies: [.product(name: "Danger", package: "swift")],
+            path: "Sources/Danger",
+            sources: ["Empty.swift"]
+        )
     ]
 )
